08ce10ed4e8341d155ecf012dbe9c42832b84a6d
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-08-13 22:23:33 +0530

    Added Images

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+ import React from "react";
+// import { v4 as uuid } from "uuid";
+ import "./App.css";
+ import Header from "./Header";
+ import AddContact from "./AddContact";
+ import ContactList from "./ContactList";
+
+
+function App () {
+   const contacts = [
+     {
+       id:"1",
+      name:"Praveen",
+      email:"praveen@gmail.com" ,
+  },
+  {
+      id:"2",
+      name:"Ram",
+      email:"Ram@gmail.com",
+  },
+  ];
+  return(
+ <div className="ui container">
+  <Header />
+  <AddContact />
+  <ContactList  contacts ={contacts} />
+
+ </div>
+  );
+}
+  // useEffect(() => {
+  //   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (retriveContacts) setContacts(retriveContacts);
+  // }, []);
+
+
+export default App;
