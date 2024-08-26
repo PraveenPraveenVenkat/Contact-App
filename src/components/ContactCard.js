0de03783271e7429e42f743dd434693ca332cfd9
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-08-26 15:40:41 +0530

    Completed React

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import user from"../images/user.png":
-const ContactCard = (props) => {
+import React from "react";
+import user from "../images/user.png";
 
-    const {id,name,email} =props.contact;
+const ContactCard = (props) => {
+  const { id, name, email } = props.contact;
   return (
-   
     <div className="item">
-        <img src='images'
-    <div className="content">
+      <img className="ui avatar image" src={user} alt="user" />
+      <div className="content">
         <div className="header">{name}</div>
         <div>{email}</div>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px" }}
+        onClick={() => props.clickHander(id)}
+      ></i>
     </div>
-    <i className="trash alternate outline icon" style={{color:"red",marginTop:"8px"}} ></i>
-</div>
-
   );
 };
 
 export default ContactCard;
+
+
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,22 +1,22 @@
- import React from "react";
+import React from "react";
 import ContactCard from "./ContactCard";
 
- const ContactList = (props) => {
+const ContactList = (props) => {
+  console.log(props);
 
-    console.log(props);
+  const deleteConactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHander={deleteConactHandler}
+        key={contact.id}
+      />
+    );
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
+};
 
-    const renderContactList = props.contacts.map((contact)=>{
-      return(
-        <ContactCard contact={contact} />;
-       
-      });
-
-    return <div className="ui celled list">
-     {renderContactList }
-    </div>;
- };
-
- export default ContactList;
-
-
-
+export default ContactList;
